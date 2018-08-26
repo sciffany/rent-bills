d537e6f7a3801d60853d32939017fe85f21b29a8
sciffany <sciffany@gmail.com>
2018-08-27 02:05:08 +0800

    signup page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def flash_class(level)
+    case level
+    when :notice then 'alert alert-success alert-dismissible'
+    when :alert then 'alert alert-danger alert-dismissible'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#hello'
   get 'pages/hi'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
