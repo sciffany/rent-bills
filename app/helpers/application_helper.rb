11d290ca1b768d63a7582e93122fec5dfac7c1a6
Tiffany Chong <tiffany.chong@transcelestial.com>
2018-08-27 16:50:12 +0800

    basic login with sessions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,10 @@ module ApplicationHelper
     when :alert then 'alert alert-danger alert-dismissible'
     end
   end
+
+ 	def current_user
+	  if session[:user_id]
+	    User.find_by(id: session[:user_id])
+	  end
+	end
 end
