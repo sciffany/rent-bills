315e9e72dde06677121830c67c28a8ab5914997c
sciffany <sciffany@gmail.com>
2019-03-30 00:21:26 +0800

    linting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-
-  #config.factory_bot.definition_file_paths = ["custom/factories"]
+  # config.factory_bot.definition_file_paths = ["custom/factories"]
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
