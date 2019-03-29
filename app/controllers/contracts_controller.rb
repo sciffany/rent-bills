fcc4c0d68457b645a50b5ae654ad213750c6a145
sciffany <sciffany@gmail.com>
2019-03-29 23:25:50 +0800

    fix contracts

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -4,22 +4,43 @@ class ContractsController < ApplicationController
   def new
     @contract = Contract.new
     @tenant = Tenant.find(params[:tenant_id])
-    @units = @contract.location.units
+    @units = @tenant.location.units
   end
 
   def create
     contract = Contract.new contract_params
     contract.tenant_id = params[:tenant_id]
     if contract.save
-      redirect_to contract_path(contract.id)
+      redirect_to tenant_url(contract.tenant_id)
     else
       redirect_to new_tenant_contract_path
       flash[:alert] = contract.errors.full_messages.join(' ')
     end
   end
 
+  def edit
+    @contract = Contract.find(params[:id])
+  end
+
   def show
     @contract = Contract.find(params[:id])
+  end
+
+  def update
+    # contract = Contract.find(params[:id])
+    # if contract.update(contract_params)
+    #   redirect_to tenant_url(contract.tenant_id),
+    #       notice: 'Contract successfully updated'
+    # else
+    #   redirect_to new_tenant_contract_path(contract_id),
+    #       alert: 'Failed to update contract:' + answer.errors.full_messages.join(', ')
+    # end
+  end
+
+  def destroy
+    contract = Contract.find(params[:id])
+    tenant_id = contract.tenant_id
+    redirect_to tenant_url(tenant_id) if contract.destroy
   end
 
   private
