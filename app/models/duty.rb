0d9ad4dbe3c8118435f32cd26656a305dce50c7e
sciffany <sciffany@gmail.com>
2019-04-24 23:44:30 +0800

    Remove duplicates and Fix Keeper Pending tab

--- app/models/duty.rb
+++ app/models/duty.rb
@@ -16,6 +16,8 @@
 class Duty < ApplicationRecord
   belongs_to :user
   belongs_to :location
+  validates :location, uniqueness: { scope: :user }
+
 
   validate :has_dates_if_temp
   validate :end_after_start
@@ -31,4 +33,21 @@ class Duty < ApplicationRecord
       errors.add(:end_date, "must be after start date")
     end
   end
+
+
+  # https://stackoverflow.com/questions/14124212/remove-duplicate-records-based-on-multiple-columns?answertab=votes#tab-top
+  def self.dedupe
+    # find all models and group them on keys which should be common
+    grouped = all.group_by{|duty| [duty.user,duty.location] }
+    grouped.values.each do |duplicates|
+      # the first one we want to keep right?
+      first_one = duplicates.shift # or pop for last one
+      # if there are any more left, they are duplicates
+      # so delete all of them
+      duplicates.each{|double| double.destroy} # duplicates can now be destroyed
+    end
+  end
+  
+  
+  Duty.dedupe
 end
