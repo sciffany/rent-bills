c23965801520610379bc491a098167f67d0a92da
sciffany <sciffany@gmail.com>
2019-04-28 16:43:48 +0800

    Edit seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,14 +11,14 @@
 #     { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-tiffany = User.create(name: 'Tiffany Chong',
-                      email: 'sciffany@gmail.com',
-                      password: '123456',
-                      role: :owner)
-dima = Location.create(name: 'Dima', password: 'dima123', user_id: tiffany.id)
-Duty.create(keeper_id: tiffany.id,
-            location_id: dima.id,
-            start_date: DateTime.parse('31/01/2018'),
-            end_date: DateTime.parse('31/01/2020'))
-Unit.create(name: 'A1', location: dima, price: 4000)
-Tenant.create(name: 'Bob', location: dima)
+# tiffany = User.create(name: 'Tiffany Chong',
+#                       email: 'sciffany@gmail.com',
+#                       password: '123456',
+#                       role: :owner)
+# dima = Location.create(name: 'Dima', password: 'dima123', user_id: tiffany.id)
+# Duty.create(user_id: tiffany.id,
+#             location_id: dima.id,
+#             start_date: DateTime.parse('31/01/2018'),
+#             end_date: DateTime.parse('31/01/2020'))
+# Unit.create(name: 'A1', location: dima, price: 4000)
+# Tenant.create(name: 'Bob', location: dima)
