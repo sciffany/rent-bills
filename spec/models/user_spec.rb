8ed7edbdee791d376c27fd48a53ae3850694962d
sciffany <sciffany@gmail.com>
2019-05-01 01:46:34 +0800

    Add confirmation to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,9 +16,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # it { should have_many(:locations) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
 
-  # it 'should have a valid factory' do
-  #   expect(build(:user)).to be_valid
-  # end
 end
